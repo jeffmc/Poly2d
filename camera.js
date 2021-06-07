ac6f6a3735a376d63e9246b12929362af4294a7e
jeff <>
2021-06-07 18:58:09 +0000

    Added more features to shader, still need a giant refactor.

--- camera.js
+++ camera.js
@@ -12,8 +12,19 @@ export class Camera {
     )
   }
   getViewProjectionMatrix(vp) {
-    // return mat4.multiply(vp, this.viewMatrix, this.projMatrix);
-    return mat4.copy(vp, this.viewMatrix);
+    mat4.fromTranslation(
+      this.viewMatrix, 
+      vec3.fromValues(
+        0,
+        0,
+        Math.sin(Date.now()*0.004)*2+2
+      )
+    );
+    mat4.fromRotation(
+      this.projMatrix, 
+      Date.now()*0.001, 
+      vec3.fromValues(0,0,1));
+    return mat4.multiply(vp, this.viewMatrix, this.projMatrix);
   }
   onResize() {
     // mat4.perspective(
--- example.js
+++ example.js
@@ -4,7 +4,7 @@ import {Shader} from "./renderer.js";
 import {Util} from "./util.js";
 import {Camera} from "./camera.js";
 
-let poly, shader, vertexBuffer, mvpLoc, colLoc, camera, mvp;
+let poly, shader, vertexBuffer, mvpLoc, colLoc, tilingLoc, camera, mvp;
 // let fBuffer, fTex;
 
 // let clear_color; 
@@ -20,24 +20,38 @@ class ExampleLayer extends Layer {
       gl.generateMipmap(gl.TEXTURE_2D);
       console.log(`${this.image.src} loaded!`);
   }
+  textureFromPixelArray(dataArray, type, width, height) {
+    let dataTypedArray = new Uint8Array(dataArray); // Don't need to do this if the data is already in a typed array
+    let texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, type, width, height, 0, type, gl.UNSIGNED_BYTE, dataTypedArray);
+    // Other texture setup here, like filter modes and mipmap generation
+    return texture;
+  }
   onAttach() {
-    // ImGui Testing
-
-    // ImGui.default();
-
-    // ImGui.CreateContext();
-    // ImGui_Impl.Init(canvas);
-
-    // ImGui.StyleColorsClassic();
-    // clear_color = new ImGui.ImVec4(0.45, 0.55, 0.60, 1.00);
-    this.texture = gl.createTexture();
-    gl.bindTexture(gl.TEXTURE_2D, this.texture);
-    
-    // Fill the texture with a 1x1 blue pixel.
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array([0, 0, 255, 255]));
+    let vals = [
+      255,255,255,
+      255,255,255,
+      255,255,255,
+      255,255,255,
+      255,255,255,
+      255,255,255,
+      255,255,255,
+      255,255,255,
+      255,255,255,
+      255,255,255,
+      255,255,255,
+      255,255,255,
+      255,255,255,
+      255,255,255,
+      255,255,255,
+      255,255,255,
+    ];
+    this.texture = this.textureFromPixelArray(
+      vals, gl.RGB, 4, 4);
     
     this.image = new Image();
-    this.image.src = "obamid.png";
+    this.image.src = "texture.png";
     this.image.addEventListener('load', this.imgLoader.bind(this));
 
 
@@ -49,25 +63,6 @@ class ExampleLayer extends Layer {
       1, 
       0.1, 1000);
     this.shader = new Shader("#vertex", "#fragment");
-
-    // Framebuffer Testing
-
-    // fBuffer = gl.createFramebuffer();
-    // gl.bindFramebuffer(gl.FRAMEBUFFER, fBuffer);
-
-    // fTex = gl.createTexture();
-    // gl.bindTexture(gl.TEXTURE_2D, fTex);
-  
-    // gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB565,
-    //               128, 128, 0,
-    //               gl.RGBA, gl.UNSIGNED_SHORT_5_6_5, null);
-  
-    // // set the filtering so we don't need mips
-    // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-    // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    
-    // gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, fTex, 0);
 
     // TODO: Move below code to a mesh class.
 
@@ -108,14 +103,12 @@ class ExampleLayer extends Layer {
     gl.vertexAttribPointer(posLoc, 3, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(posLoc);
 
-    // Load color data
+    // Load UV data
     gl.bindBuffer(gl.ARRAY_BUFFER, this.uvBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, uvs, gl.STATIC_DRAW, 0);
     let uvLoc = this.shader.attribSpecs["a_Texcoord"].location;
     gl.vertexAttribPointer(uvLoc, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(uvLoc);
-
-
 
     this.texQuadVao = gl.createVertexArray();
     gl.bindVertexArray(this.texQuadVao);
@@ -128,6 +121,7 @@ class ExampleLayer extends Layer {
     this.shader.bind();
     mvpLoc = this.shader.uniformSpecs["u_MVP"].glLocation;
     colLoc = this.shader.uniformSpecs["u_Color"].glLocation;
+    tilingLoc = this.shader.uniformSpecs["u_TilingFactor"].glLocation;
   }
 
 	onDetach() {
@@ -140,28 +134,15 @@ class ExampleLayer extends Layer {
     camera.getViewProjectionMatrix(mvp);
     gl.uniformMatrix4fv(mvpLoc, false, mvp);
 
-    gl.uniform4f(colLoc, 0.7, 1.0, 0.7, 1.0);
+    let val = Math.cos(Date.now()*0.005)*0.5+1;
+    gl.uniform4f(colLoc, 1.0, val, val, 1.0);
+    gl.uniform1f(tilingLoc, Math.sin(Date.now()*0.002)*2);
+    gl.bindTexture(gl.TEXTURE_2D, this.texture);
 
     gl.bindVertexArray(this.colorQuadVao);
 
-    // gl.bindFramebuffer(gl.FRAMEBUFFER, fBuffer);
-    // gl.viewport(0,0,100,100);
-    // gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_BYTE, 0);
 
-    // gl.bindFramebuffer(gl.FRAMEBUFFER, null);
-    // gl.viewport(0,0,gl.drawingBufferWidth,
-    //   gl.drawingBufferHeight);
-    
-    // // turn on scissor test
-    // gl.enable(gl.SCISSOR_TEST);
-
-    // // set the scissor rectangle
-    // gl.scissor(10, 10, 60, 30);
-    // gl.clearColor(1.0,0.5,0.0,1.0);
-    // gl.clear(gl.COLOR_BUFFER_BIT);
-    // // turn off scissor test again
-    // gl.disable(gl.SCISSOR_TEST);
   }
 
 	// onDebug(deltaMilliseconds) {}
